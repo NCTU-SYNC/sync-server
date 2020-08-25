be32b1224d0d0051c0420141545d77ce1a92ccd9
yslin <s5907610@gmail.com>
2020-08-25 21:24:09 +0800

    feat(): add store artcle id to firestore api

--- src/controllers/auth_controller.js
+++ src/controllers/auth_controller.js
@@ -1,4 +1,5 @@
 var firebase = require('../lib/firebase')
+var admin = require('firebase-admin')
 
 const auth = {
   async login (req, res, next) {
@@ -36,6 +37,19 @@ const auth = {
       console.log('Document data:', doc.data())
       return doc.data()
     }
+  },
+  async storeArticleIdToFirestore (req, res, next) {
+    const { uid, articleId } = req.body
+    console.log(uid)
+    const userRef = firebase.db.collection('users').doc(uid)
+    userRef.update({
+      editPostIds: admin.firestore.FieldValue.arrayUnion(articleId)
+    })
+      .then(result => res.status(200).send({
+        code: 200,
+        type: 'success'
+      }))
+      .catch(err => console.log(err))
   }
 }
 
--- src/routes/auth_router.js
+++ src/routes/auth_router.js
@@ -4,4 +4,6 @@ const ctrl = require('../controllers/auth_controller')
 router.route('/login')
   .post(ctrl.login)
 
+router.route('/storeArticleId').post(ctrl.storeArticleIdToFirestore)
+
 module.exports = router
